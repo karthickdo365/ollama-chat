9adde3e782a4c7a4da21109ad520e60680652354
karthickdo365 <karthic.cct@gmail.com__karthickdo365>
2026-03-20 10:59:50 +0530

    fix white screen

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   server: {
     port: 3000,
@@ -12,4 +13,7 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    outDir: 'dist',
+  }
 })
